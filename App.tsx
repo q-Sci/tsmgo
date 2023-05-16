9bad8c0720e9409e5f561cc7ac642bfda1acdee8
q-Sci <simon.hubert2904@gmail.com>
2023-05-16 21:46:06 +0200

    sgf >> json

--- App.tsx
+++ App.tsx
@@ -1,9 +1,12 @@
+import {useState} from 'react';
 import {
   SafeAreaView,
   StatusBar,
   StyleSheet,
+  Text,
   useColorScheme,
 } from 'react-native';
+import problems from './data/problems.json';
 
 function App(): JSX.Element {
   const isDarkMode = useColorScheme() === 'dark';
@@ -13,15 +16,23 @@ function App(): JSX.Element {
   };
 
   return (
-    <SafeAreaView style={backgroundStyle}>
+    <SafeAreaView style={styles.container}>
       <StatusBar
         barStyle={isDarkMode ? 'light-content' : 'dark-content'}
         backgroundColor={backgroundStyle.backgroundColor}
       />
+      <Text>Problem: {JSON.stringify(problems)}</Text>
     </SafeAreaView>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
 
 export default App;
